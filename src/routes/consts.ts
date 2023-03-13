123b3702718b1851ecd92f83fd361ef26bc88f9f
Lukasnvc <lukasnvc@gmail.com>
2023-03-13 20:10:13 +0200

    Add Auth routes, login page

--- src/routes/consts.ts
+++ src/routes/consts.ts
@@ -1,17 +1,28 @@
+import AuthLayouts from "../components/layouts/AuthLayouts";
 import Examples from "../pages/examples/Examples";
 import Home from "../pages/home/Home";
-import Layout from "../components/Layout";
+import Login from "../pages/login/Login";
+import MainLayout from "../components/layouts/MainLayout";
 import Pin from "../pages/pin/Pin";
 
 export const HOME_PATH = '/';
 export const PIN_PATH = '/pin';
-export const EXAMPLES_PATH = '/examples'
+export const EXAMPLES_PATH = '/examples';
 
 export const mainLayoutRoutes = {
-  Layout: Layout,
+  Layout: MainLayout,
   routes: [
     { path: HOME_PATH, Component: Home, title: 'Home' },
     { path: PIN_PATH, Component: Pin, title: 'Pin' },
-    { path: EXAMPLES_PATH, Component: Examples, title: 'Examples'}
+    { path: EXAMPLES_PATH, Component: Examples, title: 'Examples' }
+  ]
+}
+
+export const LOGIN_PATH = '/';
+
+export const authLayoutRoutes = {
+  Layout: AuthLayouts,
+  routes: [
+    { path: LOGIN_PATH, Component: Login, title: 'login'}
   ]
 }
